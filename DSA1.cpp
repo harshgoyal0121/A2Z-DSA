55a03f4fd7d23df76b0669200a7a6b0559125bad
harshgoyal0121 <harshgoyal123000@gmail.com>
2024-12-23 13:02:49 +0530

    String data type added in DSA1 and DSA2 is created

--- DSA1.cpp
+++ DSA1.cpp
@@ -2,10 +2,21 @@
 using namespace std;
 
 int main(){
-    cout<<"Enter a number: ";
-    int n;
-    cin>> n;
-    cout<<"n = "<<n<<endl;
+    // cout<<"Enter a number: ";
+    // int n;
+    // cin>> n;
+    // cout<<"n = "<<n<<endl;
 
+    cout<<"Enter a string: ";
+    string str;
+    cin>> str;
+    cout<< str<< endl;
+
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
+    cout<<"Enter another string: ";
+    string str1 ;
+    getline(cin, str1);
+    cout<<str1;
     return 0;
 }
---
+++ DSA2_IfElse.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    int age;
+    cout<<"Enter the age of that person: "<<endl;
+    cin>> age;
+
+    if(age<18 && age>0){
+        cout<<"Not an adult"<<endl;
+        cin.ignore();
+    }
+    else if(age>18){
+        cout<<"He is an adult"<<endl;
+    }
+    else{
+        cout<<"Enter a valid age!";
+    }
+}
